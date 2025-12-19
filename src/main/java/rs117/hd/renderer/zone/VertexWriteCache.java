2dc3aa1e7b7eacb9bd9bd1977093dd164b85a19d
Ruffled <105522716+RuffledPlume@users.noreply.github.com>
2025-12-19 08:15:26 +0000

    Implemented Primitive Int Maps/Set/Cache
    Reduced ProcGen Garbage
    Added Mummur Hash to reduce collisions
    Improved LinearProbe by caching the accessed key, instead of reading it twice
    
    Added IntHashSet
    
    Switched Maps over to IntHashSet since the additional value was unnecessary
    Pass in worldPos when getting tileOverride since it's already calculated
    
    Fixed Iterator generating 500MB of garbage during scene load
    
    Cache is read on multiple threads, we cannot guarantee the last alhsl value will be fully written in time
    
    Cache the last searched key result
    
    Added Bounds around multiple AABBs to allow for early out
    
    Fixed Double Linear Probe
    
    Implemented Robin Hood Hashing
    
    Dynamic Model Upload Optimizations
    
    Calculate Int Bits of Vertex position once instead of multiple times per face
    
    Added Normalized value fast path for fp32 -> fp16 conversion
    
    Calculate invLength instead of diving each axis
    
    undoVanillaShading Tweaks
    
    colorAdjust only needs to be calculated within the inner part of the if, otherwise the value isn't used
    
    We can use nDotL instead of the length
    
    Early out if value should be ignored
    
    Early out on nDotL
    
    Made FindIndex static since its consistent across all collections
    Refactored LastRead into a 32 Byte ReadCache
    
    findIndex optimizations
    
    Switch GLState IntSet to use IntHashSet & not the generic HashSet
    
    Tweaks

--- src/main/java/rs117/hd/renderer/zone/VertexWriteCache.java
+++ src/main/java/rs117/hd/renderer/zone/VertexWriteCache.java
@@ -31,56 +31,68 @@ public final class VertexWriteCache {
 			stagingBuffer = new int[min(stagingBuffer.length * 2, maxCapacity)];
 	}
 
+	public void ensureFace(int faceCount) {
+		if (stagingPosition + (9 * faceCount) > stagingBuffer.length)
+			flushAndGrow();
+	}
+
+	private void put(int value) {
+		stagingBuffer[stagingPosition++] = value;
+	}
+
 	public int putFace(
 		int alphaBiasHslA, int alphaBiasHslB, int alphaBiasHslC,
 		int materialDataA, int materialDataB, int materialDataC,
 		int terrainDataA, int terrainDataB, int terrainDataC
 	) {
-		if (stagingPosition + 9 > stagingBuffer.length)
-			flushAndGrow();
-
 		final int textureFaceIdx = (outputBuffer.position() + stagingPosition) / 3;
-		final int[] stagingBuffer = this.stagingBuffer;
-		final int stagingPosition = this.stagingPosition;
 
-		stagingBuffer[stagingPosition] = alphaBiasHslA;
-		stagingBuffer[stagingPosition + 1] = alphaBiasHslB;
-		stagingBuffer[stagingPosition + 2] = alphaBiasHslC;
+		put(alphaBiasHslA);
+		put(alphaBiasHslB);
+		put(alphaBiasHslC);
 
-		stagingBuffer[stagingPosition + 3] = materialDataA;
-		stagingBuffer[stagingPosition + 4] = materialDataB;
-		stagingBuffer[stagingPosition + 5] = materialDataC;
+		put(materialDataA);
+		put(materialDataB);
+		put(materialDataC);
 
-		stagingBuffer[stagingPosition + 6] = terrainDataA; // TODO: Remove?
-		stagingBuffer[stagingPosition + 7] = terrainDataB;
-		stagingBuffer[stagingPosition + 8] = terrainDataC;
-
-		this.stagingPosition += 9;
+		put(terrainDataA);
+		put(terrainDataB);
+		put(terrainDataC);
 
 		return textureFaceIdx;
 	}
 
+	public void ensureVertex(int vertexCount) {
+		if (stagingPosition + (7 * vertexCount) > stagingBuffer.length)
+			flushAndGrow();
+	}
+
 	public void putVertex(
 		float x, float y, float z,
-		float u, float v, float w,
+		int u, int v, int w,
 		int nx, int ny, int nz,
 		int textureFaceIdx
 	) {
-		if (stagingPosition + 7 > stagingBuffer.length)
-			flushAndGrow();
+		putVertex(
+			Float.floatToRawIntBits(x), Float.floatToRawIntBits(y), Float.floatToRawIntBits(z),
+			u, v, w,
+			nx, ny, nz,
+			textureFaceIdx);
+	}
 
-		final int[] stagingBuffer = this.stagingBuffer;
-		final int stagingPosition = this.stagingPosition;
-
-		stagingBuffer[stagingPosition] = Float.floatToRawIntBits(x);
-		stagingBuffer[stagingPosition + 1] = Float.floatToRawIntBits(y);
-		stagingBuffer[stagingPosition + 2] = Float.floatToRawIntBits(z);
-		stagingBuffer[stagingPosition + 3] = float16(v) << 16 | float16(u);
-		stagingBuffer[stagingPosition + 4] = (nx & 0xFFFF) << 16 | float16(w);
-		stagingBuffer[stagingPosition + 5] = (nz & 0xFFFF) << 16 | ny & 0xFFFF;
-		stagingBuffer[stagingPosition + 6] = textureFaceIdx;
-
-		this.stagingPosition += 7;
+	public void putVertex(
+		int x, int y, int z,
+		int u, int v, int w,
+		int nx, int ny, int nz,
+		int textureFaceIdx
+	) {
+		put( x);
+		put( y);
+		put( z);
+		put( v << 16 | u);
+		put( (nx & 0xFFFF) << 16 | w);
+		put( (nz & 0xFFFF) << 16 | ny & 0xFFFF);
+		put( textureFaceIdx);
 	}
 
 	public void flush() {
