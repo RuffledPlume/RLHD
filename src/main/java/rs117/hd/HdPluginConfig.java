f5dfbb02a83fe132160dd659c2ae5c520e3299ae
Hooder <ahooder@protonmail.com>
2026-08-09 19:22:15 +0200

    Move dither fade to misc & disable by default, as it costs 30% GPU perf

--- src/main/java/rs117/hd/HdPluginConfig.java
+++ src/main/java/rs117/hd/HdPluginConfig.java
@@ -795,22 +795,12 @@ public interface HdPluginConfig extends Config
 		return true;
 	}
 
-	String KEY_DITHER_FADING = "ditherFading";
-	@ConfigItem(
-		keyName = KEY_DITHER_FADING,
-		name = "Dither Fading",
-		description = "Whether to dither fade near plane geometry that would normally clip.",
-		position = 15,
-		section = environmentSettings
-	)
-	default boolean ditherFading() { return true; }
-
 	String KEY_HIDE_VANILLA_WATER_EFFECTS = "hideVanillaWaterEffects";
 	@ConfigItem(
 		keyName = KEY_HIDE_VANILLA_WATER_EFFECTS,
 		name = "Hide vanilla water ripples",
 		description = "Hide vanilla ripples found around objects floating in the water.",
-		position = 16,
+		position = 15,
 		section = environmentSettings
 	)
 	default boolean hideVanillaWaterEffects() { return true; }
@@ -820,7 +810,7 @@ public interface HdPluginConfig extends Config
 		keyName = KEY_POH_THEME_ENVIRONMENTS,
 		name = "Player-owned house themes",
 		description = "Change the environmental lighting based on the POH style.",
-		position = 17,
+		position = 16,
 		section = environmentSettings
 	)
 	default boolean pohThemeEnvironments() { return true; }
@@ -976,6 +966,17 @@ public interface HdPluginConfig extends Config
 		section = miscellaneousSettings
 	)
 	default boolean windowsHdrCorrection() {
+		return false;
+	}
+
+	String KEY_DITHER_FADING = "ditherFading";
+	@ConfigItem(
+		keyName = KEY_DITHER_FADING,
+		name = "Dither fading",
+		description = "Whether to dither fade things that get too close to the camera.",
+		section = miscellaneousSettings
+	)
+	default boolean ditherFading() {
 		return false;
 	}
 
@@ -1260,6 +1261,7 @@ public interface HdPluginConfig extends Config
 	default boolean multithreadedModelProcessing() {
 		return true;
 	}
+
 
 	/*====== Internal settings ======*/
 
